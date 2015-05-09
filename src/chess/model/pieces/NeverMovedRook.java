da15346b4ba458d6eb25235c68ed041cab686af2
michail <michailgames@yahoo.com>
2015-05-09 19:14:38 +0200

    Removed x an y from piece.

---
+++ src/chess/model/pieces/NeverMovedRook.java
@@ -0,0 +1,38 @@
+package chess.model.pieces;
+
+import chess.model.board.Board;
+import chess.model.board.Color;
+
+/**
+ * Projekt: Szachy
+ * Reprezentacja nigdy nie poruszonego króla
+ * Micha³ Rapacz
+ * 2015-05-09
+ */
+
+public class NeverMovedRook extends Rook {
+	
+	private final static NeverMovedRook whiteRook =
+			new NeverMovedRook(Color.WHITE);
+	private final static NeverMovedRook blackRook =
+			new NeverMovedRook(Color.BLACK);
+	
+	public static NeverMovedRook getInstance(Color color) {
+		return color == Color.WHITE ? whiteRook : blackRook;
+	}
+
+	public NeverMovedRook(Color color) {
+		super(color);
+	}
+
+	@Override
+	public Piece move(Board board, int x1, int y1, int x2, int y2) {
+		super.move(board, x1, y1, x2, y2);
+		return Rook.getInstance(getColor());
+	}
+	
+	@Override
+	public boolean canParticipateInCastling() {
+		return true;
+	}
+}
